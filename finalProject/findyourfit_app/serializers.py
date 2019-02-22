1e1996453870c02457d2fad5ced65a91d7eb6be6
nayaib98 <nayaiburgess@gmail.com>
2019-02-22 11:37:31 -0500

    completed serializers file

--- finalProject/findyourfit_app/serializers.py
+++ finalProject/findyourfit_app/serializers.py
@@ -1,2 +1,17 @@
 from rest_framework import serializers
-from .models import Post, Comment, User
+from .models import User, TrendyLook , SimilarLook
+
+class UserSerializer(serializers.ModelSeralizer):
+    class Meta:
+        model = User
+        fiels = ('username', 'password')
+
+class TrendyLookSerializer(serializers.ModelSeralizer):
+    class Meta:
+        model = TrendyLook
+        fields = ('photo_url', 'celeb_name', 'description')
+
+class SimilarLookSerializer(serializers.ModelSeralizer):
+    class Meta: 
+        model = SimilarLook
+        fields = ('photo_url', 'item_link', 'price')
