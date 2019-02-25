ccf463ea4475246a68ea5476851f1110f9368099
nayaib98 <nayaiburgess@gmail.com>
2019-02-25 16:17:07 -0500

    fixed typos, now everything is running properly

--- finalProject/findyourfit_app/models.py
+++ finalProject/findyourfit_app/models.py
@@ -4,22 +4,14 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=20)
 
-    def __str__(self):
-      return self.username
-
 class TrendyLook(models.Model):
     photo_url = models.TextField()
     celeb_name = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.photo_url
 
 class SimilarLook(models.Model):
     photo_url = models.TextField()
     item_link = models.TextField()
     price = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.photo_url
     
--- finalProject/findyourfit_app/serializers.py
+++ finalProject/findyourfit_app/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from .models import User, TrendyLook, SimilarLook
 
-class UserSerializer(serializers.ModelSeralizer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fiels = ('username', 'password')
+        fields = ('username', 'password')
 
-class TrendyLookSerializer(serializers.ModelSeralizer):
+class TrendyLookSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrendyLook
         fields = ('photo_url', 'celeb_name', 'description')
 
-class SimilarLookSerializer(serializers.ModelSeralizer):
+class SimilarLookSerializer(serializers.ModelSerializer):
     class Meta: 
         model = SimilarLook
         fields = ('photo_url', 'item_link', 'price')
