aaec899bd90ea60da31417f0487254cff25162c6
nayaib98 <nayaiburgess@gmail.com>
2019-02-25 12:43:47 -0500

    modified urls file in the app

--- finalProject/findyourfit/urls.py
+++ finalProject/findyourfit/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('hello/', include('findyourfit_app.urls')),
-    path('admin/', admin.site.urls),
+    path('api/v1/', include('findyourfit_app.urls')),
+    path('admin/', admin.site.urls), 
+    path('', include('react.urls'))
 ]
