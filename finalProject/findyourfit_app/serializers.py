b189f225043ee97d42b6e0f5737c0c0f635e0539
nayaib98 <nayaiburgess@gmail.com>
2019-02-28 18:54:07 -0500

    axios calls now work with django

--- finalProject/findyourfit_app/serializers.py
+++ finalProject/findyourfit_app/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
-from .models import User, TrendyLook, SimilarLook
+from .models import User, SimilarLook, TrendyLook 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password')
 
-class TrendyLookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TrendyLook
-        fields = ('photo_url', 'celeb_name', 'description')
-
 class SimilarLookSerializer(serializers.ModelSerializer):
     class Meta: 
         model = SimilarLook
         fields = ('photo_url', 'item_link', 'price')
+
+class TrendyLookSerializer(serializers.ModelSerializer):
+    similarlooks = SimilarLookSerializer(many=True, read_only=True)
+    class Meta:
+        model = TrendyLook
+        fields = ('photo_url', 'celeb_name', 'description', 'similarlooks')
+
