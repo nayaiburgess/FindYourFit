911ada88f37f836e600964f732ae86228c7023f1
nayaib98 <nayaiburgess@gmail.com>
2019-02-28 20:03:30 -0500

    made a user component, edited app.js and app.css

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Homepage from './Components/Homepage';
 import TrendyLook from './Components/TrendyLook';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import User from './Components/User';
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={Homepage} />
             <Route exact path="/shoptrendylooks" component={Shop} />
             <Route exact path="/viewtrendylook" component={TrendyLook}/>
+            <Route exact path="/users" component={User}/>
           </Switch>
         </Router>
       </div>
