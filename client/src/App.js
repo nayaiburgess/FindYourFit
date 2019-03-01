c0ab5d9e86fd43ef1bc998e6337a1a05e821cee1
nayaib98 <nayaiburgess@gmail.com>
2019-03-01 10:12:12 -0500

    edited App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Shop from './Components/Shop';
 import Homepage from './Components/Homepage';
 import TrendyLook from './Components/TrendyLook';
+import NewUser from './Components/NewUser';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import User from './Components/User';
@@ -16,7 +17,7 @@ class App extends Component {
             <Route exact path="/" component={Homepage} />
             <Route exact path="/shoptrendylooks" component={Shop} />
             <Route exact path="/viewtrendylook" component={TrendyLook}/>
-            <Route exact path="/users" component={User}/>
+            <Route exact path="/users" component={NewUser}/>
           </Switch>
         </Router>
       </div>
